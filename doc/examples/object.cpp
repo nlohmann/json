260745810de240cbba7c331a519af0ad77f5e95a
Dario Menendez <dmenendez@gruposantander.com>
2015-10-02 15:57:40 +0200

    fixed typos in comments for examples

--- doc/examples/object.cpp
+++ doc/examples/object.cpp
@@ -4,13 +4,13 @@ using namespace nlohmann;
 
 int main()
 {
-    // create JSON arrays
+    // create JSON objects
     json j_no_init_list = json::object();
     json j_empty_init_list = json::object({});
     json j_list_of_pairs = json::object({ {"one", 1}, {"two", 2} });
     //json j_invalid_list = json::object({ "one", 1 }); // would throw
 
-    // serialize the JSON arrays
+    // serialize the JSON objects
     std::cout << j_no_init_list << '\n';
     std::cout << j_empty_init_list << '\n';
     std::cout << j_list_of_pairs << '\n';
