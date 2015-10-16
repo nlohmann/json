bd2783f45ca91adee7b368f94651fee673844ecb
dariomt <dariomt@gmail.com>
2015-10-16 15:23:57 +0200

    Added get_ref()
    Same as get_ptr() but for references.
    If the type is incompatible it throws (get_ptr() returns null).
    Implemented in terms of get_ptr().

---
+++ doc/examples/get_ref.cpp
@@ -0,0 +1,26 @@
+#include <json.hpp>
+
+using namespace nlohmann;
+
+int main()
+{
+    // create a JSON number
+    json value = 17;
+
+    // explicitly getting references
+    auto r1 = value.get_ref<const json::number_integer_t&>();
+    auto r2 = value.get_ref<json::number_integer_t&>();
+
+    // print the values
+    std::cout << r1 << ' ' << r2 << '\n';
+    
+    // incompatible type throws exception
+    try
+    {
+        auto r3 = value.get_ref<json::number_float_t&>();
+    }
+    catch(std::domain_error& ex)
+    {
+        std::cout << ex.what() << '\n';
+    }
+}
