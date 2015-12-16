7f399f8e92bbb522b98fe98ad390ca52b462685f
Niels <niels.lohmann@gmail.com>
2015-12-16 12:12:56 +0100

    Update

--- dynsections.js
+++ dynsections.js
@@ -95,3 +95,10 @@ function toggleInherit(id)
   }
 }
 
+
+$(document).ready(function() {
+  $('.code,.codeRef').each(function() {
+    $(this).data('powertip',$('#'+$(this).attr('href').replace(/.*\//,'').replace(/[^a-z_A-Z0-9]/g,'_')).html());
+    $(this).powerTip({ placement: 's', smartPlacement: true, mouseOnToPopup: true });
+  });
+});
