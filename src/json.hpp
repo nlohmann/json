bd8db5d40e438ef501f003369e9be87aad7db2bd
Trevor Welsby <welsby.trevorr@gmail.com>
2016-01-24 17:15:44 +1000

    Remove VS induced tabs

--- src/json.hpp
+++ src/json.hpp
@@ -5539,10 +5539,10 @@ class basic_json
 
             case value_t::number_float:
             {
-				// If the number is an integer then output as a fixed with with precision 1
-				// to output "0.0", "1.0" etc as expected for some round trip tests otherwise
+                // If the number is an integer then output as a fixed with with precision 1
+                // to output "0.0", "1.0" etc as expected for some round trip tests otherwise
                 // 15 digits of precision allows round-trip IEEE 754 string->double->string; 
-				// to be safe, we read this value from std::numeric_limits<number_float_t>::digits10
+                // to be safe, we read this value from std::numeric_limits<number_float_t>::digits10
                 if (std::fmod(m_value.number_float, 1) == 0) o << std::fixed << std::setprecision(1);
                 else o << std::defaultfloat << std::setprecision(std::numeric_limits<double>::digits10);
                 o << m_value.number_float;
