c7128d75324233db6912accc24218532befd3c80
Niels Lohmann <niels.lohmann@gmail.com>
2016-12-02 20:02:39 +0100

    Update

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['emplace',['emplace',['../classnlohmann_1_1basic__json_a602f275f0359ab181221384989810604.html#a602f275f0359ab181221384989810604',1,'nlohmann::basic_json']]],
+  ['emplace_5fback',['emplace_back',['../classnlohmann_1_1basic__json_af8a435033327d9237da414afc1cce513.html#af8a435033327d9237da414afc1cce513',1,'nlohmann::basic_json']]],
   ['empty',['empty',['../classnlohmann_1_1basic__json_ae3fe0423252e171973cdd5786d036e30.html#ae3fe0423252e171973cdd5786d036e30',1,'nlohmann::basic_json']]],
   ['end',['end',['../classnlohmann_1_1basic__json_a12ccf14d39ddae52f6c7e126105a230b.html#a12ccf14d39ddae52f6c7e126105a230b',1,'nlohmann::basic_json::end() noexcept'],['../classnlohmann_1_1basic__json_abd7e95159740e94160c13392b1536eb1.html#abd7e95159740e94160c13392b1536eb1',1,'nlohmann::basic_json::end() const noexcept']]],
   ['erase',['erase',['../classnlohmann_1_1basic__json_ab9f8c9a02d6bb794ee26801a232ca4f4.html#ab9f8c9a02d6bb794ee26801a232ca4f4',1,'nlohmann::basic_json::erase(IteratorType pos)'],['../classnlohmann_1_1basic__json_aa8996e3f8cd3e5113dcc3cb34bef7c0a.html#aa8996e3f8cd3e5113dcc3cb34bef7c0a',1,'nlohmann::basic_json::erase(IteratorType first, IteratorType last)'],['../classnlohmann_1_1basic__json_aa36e72ffc3241b960fe9186d19e03bc3.html#aa36e72ffc3241b960fe9186d19e03bc3',1,'nlohmann::basic_json::erase(const typename object_t::key_type &amp;key)'],['../classnlohmann_1_1basic__json_a3da254c422ede5495f2815c5e48c00c5.html#a3da254c422ede5495f2815c5e48c00c5',1,'nlohmann::basic_json::erase(const size_type idx)']]]
