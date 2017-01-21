40ba5a8b7b4dffce2a24e43a4a108a04756dbfc6
Théo DELRIEU <theo@tanker.io>
2017-01-21 16:45:08 +0100

    remove inline keyword from anonymous namespace (not needed)

--- src/json.hpp
+++ src/json.hpp
@@ -870,7 +870,7 @@ struct DecimalSeparator : std::numpunct<char>
 };
 }
 
-inline namespace
+namespace
 {
 constexpr const auto& to_json = detail::static_const<detail::to_json_fn>::value;
 constexpr const auto& from_json = detail::static_const<detail::from_json_fn>::value;
