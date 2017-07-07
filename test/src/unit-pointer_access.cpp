63ecbfd36be3087d24bca385370413869900ff35
Niels Lohmann <mail@nlohmann.me>
2017-07-07 23:38:04 +0200

    :white_check_mark: re-added test
    I have no idea why this test has been removed in the first place.

--- test/src/unit-pointer_access.cpp
+++ test/src/unit-pointer_access.cpp
@@ -257,11 +257,11 @@ TEST_CASE("pointer access")
 
         const test_type* p2 = value.get_ptr<const test_type*>();
         CHECK(p1 == value.get_ptr<const test_type*>());
-        //CHECK(*p2 == value.get<test_type>());
+        CHECK(*p2 == value.get<test_type>());
 
         const test_type* const p3 = value.get_ptr<const test_type* const>();
         CHECK(p1 == value.get_ptr<const test_type* const>());
-        //CHECK(*p3 == value.get<test_type>());
+        CHECK(*p3 == value.get<test_type>());
 
         // check if null pointers are returned correctly
         CHECK(value.get_ptr<const json::object_t*>() == nullptr);
