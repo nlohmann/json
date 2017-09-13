b90529c36d87348f8ea2ea2ac13f63ffc52ff35a
Niels Lohmann <mail@nlohmann.me>
2017-09-13 18:56:54 +0200

    :white_check_mark: improved test coverage

--- test/src/unit-unicode.cpp
+++ test/src/unit-unicode.cpp
@@ -924,6 +924,40 @@ TEST_CASE("Unicode", "[hide]")
             }
         }
 
+        SECTION("incorrect sequences")
+        {
+            SECTION("incorrect surrogate values")
+            {
+                CHECK_THROWS_AS(json::parse("\"\\uDC00\\uDC00\""), json::parse_error&);
+                CHECK_THROWS_WITH(json::parse("\"\\uDC00\\uDC00\""),
+                                  "[json.exception.parse_error.101] parse error at 7: syntax error - invalid string: surrogate U+DC00..U+DFFF must follow U+D800..U+DBFF; last read: '\"\\uDC00'");
+
+                CHECK_THROWS_AS(json::parse("\"\\uD7FF\\uDC00\""), json::parse_error&);
+                CHECK_THROWS_WITH(json::parse("\"\\uD7FF\\uDC00\""),
+                                  "[json.exception.parse_error.101] parse error at 13: syntax error - invalid string: surrogate U+DC00..U+DFFF must follow U+D800..U+DBFF; last read: '\"\\uD7FF\\uDC00'");
+
+                CHECK_THROWS_AS(json::parse("\"\\uD800]\""), json::parse_error&);
+                CHECK_THROWS_WITH(json::parse("\"\\uD800]\""),
+                                  "[json.exception.parse_error.101] parse error at 8: syntax error - invalid string: surrogate U+DC00..U+DFFF must be followed by U+DC00..U+DFFF; last read: '\"\\uD800]'");
+
+                CHECK_THROWS_AS(json::parse("\"\\uD800\\v\""), json::parse_error&);
+                CHECK_THROWS_WITH(json::parse("\"\\uD800\\v\""),
+                                  "[json.exception.parse_error.101] parse error at 9: syntax error - invalid string: surrogate U+DC00..U+DFFF must be followed by U+DC00..U+DFFF; last read: '\"\\uD800\\v'");
+
+                CHECK_THROWS_AS(json::parse("\"\\uD800\\u123\""), json::parse_error&);
+                CHECK_THROWS_WITH(json::parse("\"\\uD800\\u123\""),
+                                  "[json.exception.parse_error.101] parse error at 13: syntax error - invalid string: '\\u' must be followed by 4 hex digits; last read: '\"\\uD800\\u123\"'");
+
+                CHECK_THROWS_AS(json::parse("\"\\uD800\\uDBFF\""), json::parse_error&);
+                CHECK_THROWS_WITH(json::parse("\"\\uD800\\uDBFF\""),
+                                  "[json.exception.parse_error.101] parse error at 13: syntax error - invalid string: surrogate U+DC00..U+DFFF must be followed by U+DC00..U+DFFF; last read: '\"\\uD800\\uDBFF'");
+
+                CHECK_THROWS_AS(json::parse("\"\\uD800\\uE000\""), json::parse_error&);
+                CHECK_THROWS_WITH(json::parse("\"\\uD800\\uE000\""),
+                                  "[json.exception.parse_error.101] parse error at 13: syntax error - invalid string: surrogate U+DC00..U+DFFF must be followed by U+DC00..U+DFFF; last read: '\"\\uD800\\uE000'");
+            }
+        }
+
 #if 0
         SECTION("incorrect sequences")
         {
