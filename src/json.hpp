73b1629a15a66a3e15d456f400ea9ceca4d2e317
Jamie Seward <jamie.d.seward@gmail.com>
2017-10-23 23:45:39 -0700

    Remove tabs for spaces

--- src/json.hpp
+++ src/json.hpp
@@ -111,10 +111,10 @@ SOFTWARE.
 
 // cpp language standard detection
 #if (defined(__cplusplus) && __cplusplus >= 201703L) || (defined(_HAS_CXX17) && _HAS_CXX17 == 1) // fix for issue #464
-	#define JSON_HAS_CPP_17
-	#define JSON_HAS_CPP_14
+    #define JSON_HAS_CPP_17
+    #define JSON_HAS_CPP_14
 #elif (defined(__cplusplus) && __cplusplus >= 201402L) || (defined(_HAS_CXX14) && _HAS_CXX14 == 1)
-	#define JSON_HAS_CPP_14
+    #define JSON_HAS_CPP_14
 #endif
 
 /*!
