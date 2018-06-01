4778c02ab5e224cb463f27158b6cf1d79732acf4
Guillaume Racicot <gufideg@gmail.com>
2018-06-01 14:42:25 -0400

    Set MSVC version from 1514 and older

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -12470,7 +12470,7 @@ class basic_json
                    not detail::is_basic_json<ValueType>::value
 #ifndef _MSC_VER  // fix for issue #167 operator<< ambiguity under VS2015
                    and not std::is_same<ValueType, std::initializer_list<typename string_t::value_type>>::value
-#if defined(JSON_HAS_CPP_17) && _MSC_VER <= 1913
+#if defined(JSON_HAS_CPP_17) && _MSC_VER <= 1914
                    and not std::is_same<ValueType, typename std::string_view>::value
 #endif
 #endif
