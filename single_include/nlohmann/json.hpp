1ea8cd128c11746d9ee18af1dc4b3d3d516b7296
Théo DELRIEU <theo.delrieu@tanker.io>
2018-09-07 12:15:35 +0200

    fix void_t for older compilers

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -3572,8 +3572,11 @@ namespace nlohmann
 {
 namespace detail
 {
-template <typename...>
-using void_t = void;
+template <typename ...Ts> struct make_void
+{
+    using type = void;
+};
+template <typename ...Ts> using void_t = typename make_void<Ts...>::type;
 }
 }
 
