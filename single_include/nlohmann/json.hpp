6d34d64bfdb1b06e5a80ac14b35abddd0006d9c5
Niels Lohmann <mail@nlohmann.me>
2018-10-08 06:54:51 +0200

    :ambulance: fixed compilation error

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -10375,10 +10375,10 @@ class json_ref
     {}
 
     // class should be movable only
-    json_ref(json_ref&&) noexcept = default;
+    json_ref(json_ref&&) = default;
     json_ref(const json_ref&) = delete;
     json_ref& operator=(const json_ref&) = delete;
-    json_ref& operator=(json_ref&&) noexcept = default;
+    json_ref& operator=(json_ref&&) = delete;
     ~json_ref() = default;
 
     value_type moved_or_copied() const
