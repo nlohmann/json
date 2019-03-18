e236b62994fbf1ef5745e04fca034dc7e3615335
jipr311 <jipr311@gmail.com>
2019-03-18 16:50:13 +0100

    Update json.hpp
    year of license updated.

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -6,7 +6,7 @@
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 SPDX-License-Identifier: MIT
-Copyright (c) 2013-2018 Niels Lohmann <http://nlohmann.me>.
+Copyright (c) 2013-2019 Niels Lohmann <http://nlohmann.me>.
 
 Permission is hereby  granted, free of charge, to any  person obtaining a copy
 of this software and associated  documentation files (the "Software"), to deal
