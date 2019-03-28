d21d298397d349a4a4ade24a5012277199f2d63f
Niels Lohmann <mail@nlohmann.me>
2019-03-28 14:23:26 +0100

    :art: fixed indentation

--- include/nlohmann/detail/iterators/iterator_traits.hpp
+++ include/nlohmann/detail/iterators/iterator_traits.hpp
@@ -13,15 +13,16 @@ template <typename It, typename = void>
 struct iterator_types {};
 
 template <typename It>
-struct iterator_types<
+struct iterator_types <
     It,
     void_t<typename It::difference_type, typename It::value_type, typename It::pointer,
-           typename It::reference, typename It::iterator_category>> {
-  using difference_type = typename It::difference_type;
-  using value_type = typename It::value_type;
-  using pointer = typename It::pointer;
-  using reference = typename It::reference;
-  using iterator_category = typename It::iterator_category;
+    typename It::reference, typename It::iterator_category >>
+{
+    using difference_type = typename It::difference_type;
+    using value_type = typename It::value_type;
+    using pointer = typename It::pointer;
+    using reference = typename It::reference;
+    using iterator_category = typename It::iterator_category;
 };
 
 // This is required as some compilers implement std::iterator_traits in a way that
@@ -32,18 +33,19 @@ struct iterator_traits
 };
 
 template <typename T>
-struct iterator_traits<T, enable_if_t<!std::is_pointer<T>::value>>
-  : iterator_types<T>
+struct iterator_traits < T, enable_if_t < !std::is_pointer<T>::value >>
+            : iterator_types<T>
 {
 };
 
 template <typename T>
-struct iterator_traits<T*, enable_if_t<std::is_object<T>::value>> {
-  using iterator_category = std::random_access_iterator_tag;
-  using value_type = T;
-  using difference_type = ptrdiff_t;
-  using pointer = T*;
-  using reference = T&;
+struct iterator_traits<T*, enable_if_t<std::is_object<T>::value>>
+{
+    using iterator_category = std::random_access_iterator_tag;
+    using value_type = T;
+    using difference_type = ptrdiff_t;
+    using pointer = T*;
+    using reference = T&;
 };
 } // namespace detail
 } // namespace nlohmann
