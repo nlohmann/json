23635704c3f9fce7d19eeca018277022c11a7b6b
Niels Lohmann <mail@nlohmann.me>
2019-03-28 14:23:07 +0100

    :arrow_up: added script to update cpplint

---
+++ third_party/cpplint/update.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget -N https://raw.githubusercontent.com/cpplint/cpplint/master/cpplint.py
+wget -N https://raw.githubusercontent.com/cpplint/cpplint/master/LICENSE
+wget -N https://raw.githubusercontent.com/cpplint/cpplint/master/README.rst
