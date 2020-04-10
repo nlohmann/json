6121fc52cf9d29bcd6a599c9390c4774fb626de6
Niels Lohmann <mail@nlohmann.me>
2020-04-10 13:22:58 +0200

    :art: fix indentation

--- test/src/unit-allocator.cpp
+++ test/src/unit-allocator.cpp
@@ -245,12 +245,13 @@ struct allocator_no_forward : std::allocator<T>
     allocator_no_forward(allocator_no_forward<U>) {}
 
     template <class U>
-    struct rebind {
+    struct rebind
+    {
         using other =  allocator_no_forward<U>;
     };
 
     template <class... Args>
-    void construct(T* p, const Args&... args)
+    void construct(T* p, const Args& ... args)
     {
         // force copy even if move is available
         ::new (static_cast<void*>(p)) T(args...);
@@ -263,16 +264,16 @@ TEST_CASE("bad my_allocator::construct")
     SECTION("my_allocator::construct doesn't forward")
     {
         using bad_alloc_json = nlohmann::basic_json<std::map,
-                                                    std::vector,
-                                                    std::string,
-                                                    bool,
-                                                    std::int64_t,
-                                                    std::uint64_t,
-                                                    double,
-                                                    allocator_no_forward>;
+              std::vector,
+              std::string,
+              bool,
+              std::int64_t,
+              std::uint64_t,
+              double,
+              allocator_no_forward>;
 
-      bad_alloc_json json;
-      json["test"] = bad_alloc_json::array_t();
-      json["test"].push_back("should not leak");
+        bad_alloc_json json;
+        json["test"] = bad_alloc_json::array_t();
+        json["test"].push_back("should not leak");
     }
 }
