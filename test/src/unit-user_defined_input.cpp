897061c434563559bf9014f1e7e53962e928aec2
Francois Chabot <francois@ai-r.com>
2020-05-28 10:11:19 -0400

    amalgamate

--- test/src/unit-user_defined_input.cpp
+++ test/src/unit-user_defined_input.cpp
@@ -98,7 +98,7 @@ TEST_CASE("Custom iterator")
         {
             return *ptr;
         }
-        
+
         bool operator!=(const MyIterator& rhs) const
         {
             return ptr != rhs.ptr;
