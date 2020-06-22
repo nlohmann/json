a9809f3381d4eb03d54fa2dddb23753903678f93
Niels Lohmann <mail@nlohmann.me>
2020-06-22 23:02:28 +0200

    :checkered_flag: revert change that breaks with MSVC

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -15606,7 +15606,7 @@ class serializer
         }
 
         // use a pointer to fill the buffer
-        auto* buffer_ptr = number_buffer.begin();
+        auto buffer_ptr = number_buffer.begin();
 
         const bool is_negative = std::is_same<NumberType, number_integer_t>::value and not(x >= 0); // see issue #755
         number_unsigned_t abs_value;
