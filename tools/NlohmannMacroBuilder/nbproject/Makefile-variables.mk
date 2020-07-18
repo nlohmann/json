8730af0788c6c0846908a82efa01d52ab48cd268
pf <peter.f.j.featherstone@bath.edu>
2020-07-18 16:10:32 +0100

    [JSON] 	- dirty code for building intrusive and non-intrusive serialisation/deserialisation macros for custom types 		- netbeans project configurations

---
+++ tools/NlohmannMacroBuilder/nbproject/Makefile-variables.mk
@@ -0,0 +1,27 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=nlohmannmacrobuilder
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/nlohmannmacrobuilder
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=nlohmannmacrobuilder.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/nlohmannmacrobuilder.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
