b3e5cb7f20dcc5c806e418df34324eca60d17d4e
Niels Lohmann <mail@nlohmann.me>
2020-08-06 13:45:29 +0200

    :bookmark: set version to 3.9.1

--- test/src/unit-meta.cpp
+++ test/src/unit-meta.cpp
@@ -1,7 +1,7 @@
 /*
     __ _____ _____ _____
  __|  |   __|     |   | |  JSON for Modern C++ (test suite)
-|  |  |__   |  |  | | | |  version 3.9.0
+|  |  |__   |  |  | | | |  version 3.9.1
 |_____|_____|_____|_|___|  https://github.com/nlohmann/json
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>.
@@ -43,10 +43,10 @@ TEST_CASE("version information")
         CHECK(j["url"] == "https://github.com/nlohmann/json");
         CHECK(j["version"] == json(
         {
-            {"string", "3.9.0"},
+            {"string", "3.9.1"},
             {"major", 3},
             {"minor", 9},
-            {"patch", 0}
+            {"patch", 1}
         }));
 
         CHECK(j.find("platform") != j.end());
