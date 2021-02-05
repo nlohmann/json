d2852ea0c1228826ba5a2b3070630312b24f2280
Alexey Ochapov <alexez@alexez.com>
2021-02-05 20:09:38 +0300

    add test for enum and ordered_json

--- test/src/unit-conversions.cpp
+++ test/src/unit-conversions.cpp
@@ -32,6 +32,7 @@ SOFTWARE.
 #define JSON_TESTS_PRIVATE
 #include <nlohmann/json.hpp>
 using nlohmann::json;
+using nlohmann::ordered_json;
 
 #include <deque>
 #include <forward_list>
@@ -1684,6 +1685,9 @@ TEST_CASE("JSON to enum mapping")
 
         // invalid json -> first enum
         CHECK(cards::kreuz == json("what?").get<cards>());
+
+        // issue #2491, fixed in #2624
+        CHECK(ordered_json(cards::kreuz) == "kreuz");
     }
 
     SECTION("traditional enum")
