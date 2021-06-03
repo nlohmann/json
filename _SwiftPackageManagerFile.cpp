569a5dee769290ca20fec2b5b9e660d67bc2129c
Richard Topchii <topchiy@protonmail.ch>
2021-06-03 13:43:27 +0300

    Add empty file to trigger Swift Package Manager to build json module

---
+++ _SwiftPackageManagerFile.cpp
@@ -0,0 +1,5 @@
+// This file exists only to trigger the json module to build; without
+// it swiftpm won't build anything, and then the package is not available for
+// the modules that need it.
+
+#include <json.hpp>
