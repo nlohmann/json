e41e3151647a203b42cbe0c507506beba8cb697b
Raphael Grimm <raphael.grimm@kit.edu>
2021-11-01 12:23:49 +0100

    Add NOLINT since clang-tidy requests make_unique, but C++11 jobs don't support it

--- tests/src/unit-custom-base-class.cpp
+++ tests/src/unit-custom-base-class.cpp
@@ -173,7 +173,7 @@ TEST_CASE("JSON Node Metadata")
     {
         using json = json_with_metadata<std::unique_ptr<int>>;
         json value;
-        value.metadata().reset(new int(42));
+        value.metadata().reset(new int(42)); // NOLINT
         auto moved = std::move(value);
 
         CHECK(moved.metadata() != nullptr);
