7b60a2a6d9dcbac9236efbad4fae2072a6eb8fc7
Niels Lohmann <mail@nlohmann.me>
2021-12-30 09:19:32 +0100

    :construction_worker: add CI step for ICPC

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -840,7 +840,7 @@ endforeach()
 ###############################################################################
 
 add_custom_target(ci_icpc
-    COMMAND source /opt/intel/oneapi/setvars.sh
+    COMMAND . /opt/intel/oneapi/setvars.sh
     COMMAND ${CMAKE_COMMAND}
         -DCMAKE_BUILD_TYPE=Debug -GNinja
         -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc
