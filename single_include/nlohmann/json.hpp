52e16a9f9c540a2de963a7b847e3f1611c71567f
Niels Lohmann <mail@nlohmann.me>
2022-04-06 08:29:01 +0200

    :children_crossing: add check if different version is also included #3337 (#3418)

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -39,6 +39,14 @@ SOFTWARE.
 #ifndef INCLUDE_NLOHMANN_JSON_HPP_
 #define INCLUDE_NLOHMANN_JSON_HPP_
 
+#ifndef JSON_SKIP_LIBRARY_VERSION_CHECK
+    #if defined(NLOHMANN_JSON_VERSION_MAJOR) && defined(NLOHMANN_JSON_VERSION_MINOR) && defined(NLOHMANN_JSON_VERSION_PATCH)
+        #if NLOHMANN_JSON_VERSION_MAJOR != 3 || NLOHMANN_JSON_VERSION_MINOR != 10 || NLOHMANN_JSON_VERSION_PATCH != 5
+            #warning "Already included a different version of the library!"
+        #endif
+    #endif
+#endif
+
 #define NLOHMANN_JSON_VERSION_MAJOR 3
 #define NLOHMANN_JSON_VERSION_MINOR 10
 #define NLOHMANN_JSON_VERSION_PATCH 5
