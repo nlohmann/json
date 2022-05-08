5f273119d9aac641d15f8cb78d4ece60f927ed21
Niels Lohmann <mail@nlohmann.me>
2022-05-08 18:04:32 +0200

    :memo: fix wording in example

--- docs/examples/object_comparator_t.cpp
+++ docs/examples/object_comparator_t.cpp
@@ -6,6 +6,6 @@ using json = nlohmann::json;
 int main()
 {
     std::cout << std::boolalpha
-              << "one < two : " << json::object_comparator_t{}("one", "two") << "\n"
-              << "three < four : " << json::object_comparator_t{}("three", "four") << std::endl;
+              << "json::object_comparator_t(\"one\", \"two\") = " << json::object_comparator_t{}("one", "two") << "\n"
+              << "json::object_comparator_t(\"three\", \"four\") = " << json::object_comparator_t{}("three", "four") << std::endl;
 }
