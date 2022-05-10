892247dd8e612c4198b273aba9dcdfdd269f868f
Niels Lohmann <mail@nlohmann.me>
2022-05-10 21:07:50 +0200

    :art: fix indentation

--- docs/examples/get_allocator.cpp
+++ docs/examples/get_allocator.cpp
@@ -8,7 +8,7 @@ int main()
     auto alloc = json::get_allocator();
     using traits_t = std::allocator_traits<decltype(alloc)>;
 
-    json *j = traits_t::allocate(alloc, 1);
+    json* j = traits_t::allocate(alloc, 1);
     traits_t::construct(alloc, j, "Hello, world!");
 
     std::cout << *j << std::endl;
