f4adc22b382e5a5215d7fd5f6e0e396a3c55a972
Kunal Tyagi <tyagi.kunal@live.com>
2022-06-26 02:01:27 +0900

    Fix accidental change

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -18627,7 +18627,7 @@ class basic_json // NOLINT(cppcoreguidelines-special-member-functions,hicpp-spec
         detail::parser_callback_t<basic_json>cb = nullptr,
         const bool allow_exceptions = true,
         const bool ignore_comments = false
-    )
+                                 )
     {
         return ::nlohmann::detail::parser<basic_json, InputAdapterType>(std::move(adapter),
                 std::move(cb), allow_exceptions, ignore_comments);
