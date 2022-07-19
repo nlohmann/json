adbd67fc6ce0fd0f4b7db395818429ec28d4cd41
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2022-07-19 08:10:47 +0200

    Encode ABI information in inline namespace
    Add _diag suffix to inline namespace if JSON_DIAGNOSTICS is enabled, and
    _ldvcmp suffix if JSON_USE_LEGACY_DISCARDED_VALUE_COMPARISON is enabled.

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -2517,13 +2517,20 @@ JSON_HEDLEY_DIAGNOSTIC_POP
     #define NLOHMANN_JSON_ABI_TAG_DIAGNOSTICS
 #endif
 
+#if JSON_USE_LEGACY_DISCARDED_VALUE_COMPARISON
+    #define NLOHMANN_JSON_ABI_TAG_LEGACY_DISCARDED_VALUE_COMPARISON _ldvcmp
+#else
+    #define NLOHMANN_JSON_ABI_TAG_LEGACY_DISCARDED_VALUE_COMPARISON
+#endif
+
 #define NLOHMANN_JSON_ABI_PREFIX json_v3_10_5
-#define NLOHMANN_JSON_ABI_CONCAT_EX(a, b) a ## b
-#define NLOHMANN_JSON_ABI_CONCAT(a, b) NLOHMANN_JSON_ABI_CONCAT_EX(a, b)
-#define NLOHMANN_JSON_ABI_STRING              \
-    NLOHMANN_JSON_ABI_CONCAT(                 \
-            NLOHMANN_JSON_ABI_PREFIX,         \
-            NLOHMANN_JSON_ABI_TAG_DIAGNOSTICS \
+#define NLOHMANN_JSON_ABI_CONCAT_EX(a, b, c) a ## b ## c
+#define NLOHMANN_JSON_ABI_CONCAT(a, b, c) NLOHMANN_JSON_ABI_CONCAT_EX(a, b, c)
+#define NLOHMANN_JSON_ABI_STRING                                    \
+    NLOHMANN_JSON_ABI_CONCAT(                                       \
+            NLOHMANN_JSON_ABI_PREFIX,                               \
+            NLOHMANN_JSON_ABI_TAG_DIAGNOSTICS,                      \
+            NLOHMANN_JSON_ABI_TAG_LEGACY_DISCARDED_VALUE_COMPARISON \
                             )
 #define NLOHMANN_JSON_NAMESPACE nlohmann::NLOHMANN_JSON_ABI_STRING
 
