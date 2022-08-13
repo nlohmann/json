f9003d169b286accbfecbabeaa57a459f83c8eeb
Niels Lohmann <mail@nlohmann.me>
2022-08-13 13:36:08 +0200

    :art: fix indentation

--- tests/src/unit-bon8.cpp
+++ tests/src/unit-bon8.cpp
@@ -32,7 +32,7 @@ SOFTWARE.
 #include <nlohmann/json.hpp>
 using nlohmann::json;
 #ifdef JSON_TEST_NO_GLOBAL_UDLS
-using namespace nlohmann::literals; // NOLINT(google-build-using-namespace)
+    using namespace nlohmann::literals; // NOLINT(google-build-using-namespace)
 #endif
 
 #include <iostream>
