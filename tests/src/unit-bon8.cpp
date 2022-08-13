b116faa02df582d72badc2d906290d40c1a0b979
Niels Lohmann <mail@nlohmann.me>
2022-08-13 13:29:11 +0200

    :green_heart: fix unit test

--- tests/src/unit-bon8.cpp
+++ tests/src/unit-bon8.cpp
@@ -31,6 +31,9 @@ SOFTWARE.
 
 #include <nlohmann/json.hpp>
 using nlohmann::json;
+#ifdef JSON_TEST_NO_GLOBAL_UDLS
+using namespace nlohmann::literals; // NOLINT(google-build-using-namespace)
+#endif
 
 #include <iostream>
 #include <fstream>
--- tests/src/unit-bson.cpp
+++ tests/src/unit-bson.cpp
@@ -10,9 +10,6 @@
 
 #include <nlohmann/json.hpp>
 using nlohmann::json;
-#ifdef JSON_TEST_NO_GLOBAL_UDLS
-using namespace nlohmann::literals; // NOLINT(google-build-using-namespace)
-#endif
 
 #include <fstream>
 #include <sstream>
