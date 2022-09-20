6724d9450fa8e6d8301d426111799c4084281911
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2022-09-20 16:08:54 +0200

    Allow value(json_pointer) to work with arrays as documented

--- include/nlohmann/json.hpp
+++ include/nlohmann/json.hpp
@@ -2320,8 +2320,8 @@ class basic_json // NOLINT(cppcoreguidelines-special-member-functions,hicpp-spec
                    && !std::is_same<value_t, detail::uncvref_t<ValueType>>::value, int > = 0 >
     ValueType value(const json_pointer& ptr, const ValueType& default_value) const
     {
-        // value only works for objects
-        if (JSON_HEDLEY_LIKELY(is_object()))
+        // value only works for arrays and objects
+        if (JSON_HEDLEY_LIKELY(is_structured()))
         {
             // if pointer resolves a value, return it or use default value
             JSON_TRY
@@ -2345,8 +2345,8 @@ class basic_json // NOLINT(cppcoreguidelines-special-member-functions,hicpp-spec
                    && !std::is_same<value_t, detail::uncvref_t<ValueType>>::value, int > = 0 >
     ReturnType value(const json_pointer& ptr, ValueType && default_value) const
     {
-        // value only works for objects
-        if (JSON_HEDLEY_LIKELY(is_object()))
+        // value only works for arrays and objects
+        if (JSON_HEDLEY_LIKELY(is_structured()))
         {
             // if pointer resolves a value, return it or use default value
             JSON_TRY
