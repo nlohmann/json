877d21a233c11d750d335b5ba6671c5e27999f6b
Sven Fink <sven.fink@wipotec.com>
2023-01-12 15:40:47 +0100

    PrettyPrinter: Check if match is valid before accessing group

--- tools/gdb_pretty_printer/nlohmann-json.py
+++ tools/gdb_pretty_printer/nlohmann-json.py
@@ -14,19 +14,19 @@ class JsonValuePrinter:
         return self.val
 
 def json_lookup_function(val):
-    m = ns_pattern.fullmatch(val.type.strip_typedefs().name)
-    name = m.group('name')
-    if name and name.startswith('basic_json<') and name.endswith('>'):
-        m = ns_pattern.fullmatch(str(val['m_type']))
-        t = m.group('name')
-        if t and t.startswith('detail::value_t::'):
-            try:
-                union_val = val['m_value'][t.removeprefix('detail::value_t::')]
-                if union_val.type.code == gdb.TYPE_CODE_PTR:
-                    return gdb.default_visualizer(union_val.dereference())
-                else:
-                    return JsonValuePrinter(union_val)
-            except Exception:
-                return JsonValuePrinter(val['m_type'])
+    if m := ns_pattern.fullmatch(str(val.type.strip_typedefs().name)):
+      name = m.group('name')
+      if name and name.startswith('basic_json<') and name.endswith('>'):
+          m = ns_pattern.fullmatch(str(val['m_type']))
+          t = m.group('name')
+          if t and t.startswith('detail::value_t::'):
+              try:
+                  union_val = val['m_value'][t.removeprefix('detail::value_t::')]
+                  if union_val.type.code == gdb.TYPE_CODE_PTR:
+                      return gdb.default_visualizer(union_val.dereference())
+                  else:
+                      return JsonValuePrinter(union_val)
+              except Exception:
+                  return JsonValuePrinter(val['m_type'])
 
 gdb.pretty_printers.append(json_lookup_function)
