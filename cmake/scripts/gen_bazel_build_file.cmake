7cb526e4da4892d68a0938383849e1bb26ff2857
Thi Doãn <t@thi.im>
2023-03-15 13:17:19 +0900

    Only expose `include/nlohmann/json.hpp` as public header
    All other headers are not meant to be included by a client of this
    library and should be declared in `srcs`.

--- cmake/scripts/gen_bazel_build_file.cmake
+++ cmake/scripts/gen_bazel_build_file.cmake
@@ -4,11 +4,12 @@ set(PROJECT_ROOT "${CMAKE_CURRENT_LIST_DIR}/../..")
 set(BUILD_FILE "${PROJECT_ROOT}/BUILD.bazel")
 
 file(GLOB_RECURSE HEADERS LIST_DIRECTORIES false RELATIVE "${PROJECT_ROOT}" "include/*.hpp")
+list(REMOVE_ITEM HEADERS "include/nlohmann/json.hpp")
 
 file(WRITE "${BUILD_FILE}" [=[
 cc_library(
     name = "json",
-    hdrs = [
+    srcs = [
 ]=])
 
 foreach(header ${HEADERS})
@@ -17,6 +18,7 @@ endforeach()
 
 file(APPEND "${BUILD_FILE}" [=[
     ],
+    hdrs = ["include/nlohmann/json.hpp"],
     includes = ["include"],
     visibility = ["//visibility:public"],
     alwayslink = True,
