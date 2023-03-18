bc46595053526ea3b3b31838b424c1de7009ef45
Niels Lohmann <mail@nlohmann.me>
2023-03-18 15:21:07 +0100

    :alembic: try to fix compilation issue

--- tests/src/unit-constructor1.cpp
+++ tests/src/unit-constructor1.cpp
@@ -279,6 +279,17 @@ TEST_CASE("constructors")
             const auto t = j.get<std::tuple<int, float, std::string>>();
             CHECK(std::get<0>(t) == j[0]);
             CHECK(std::get<1>(t) == j[1]);
+            const bool res = (std::get<2>(t) == j[2]);
+            CHECK(res);
+        }
+
+        SECTION("std::tuple with discarded values")
+        {
+            json const j{1, 2.0, std::string{"string"}, 42};
+
+            const auto t = j.get<std::tuple<int, float, std::string>>();
+            CHECK(std::get<0>(t) == j[0]);
+            CHECK(std::get<1>(t) == j[1]);
             CHECK(std::get<2>(t) == j[2]);
         }
 
