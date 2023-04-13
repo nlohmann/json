f26d9e518d3213e73f8b7ea9055c266f5a6c1fe3
Aleksei <sapial@f-secure.com>
2023-04-13 20:16:33 +0300

    Support Apple's Swift Package Manager
    **Reason**
    - Main way to develop under Apple's platforms is using SPM
    
    **Changes**
    - Added Package.swift
    - Added _SwiftPackageManagerFile.cpp to single_include/nlohmann in order SPM to see it as a C++ source file
    - Added .build to .gitignore
    
    **Context**
    - SPM is a package manager for Swift and C-based languages. It is the main way to develop under Apple's platforms. It is also used to build Swift packages for Linux.

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "nlohmann-json",
+     platforms: [
+        .iOS(.v11), .macOS(.v10_13), .tvOS(.v11), .watchOS(.v4)
+    ],
+    products: [
+        .library(name: "nlohmann-json", targets: ["nlohmann-json"]),
+    ],
+    targets: [
+        .target(
+            name: "nlohmann-json",
+            path: "single_include/nlohmann",
+            publicHeadersPath: "."
+        ),
+    ],
+    cxxLanguageStandard: .cxx11
+)
