c10d610054c7217a196e7b827dea79ed6fa242b8
Mohammad Reza Gharabaghi <40915667+mrgharabaghi@users.noreply.github.com>
2023-11-27 06:15:34 +0100

    Fix examples/json_lines.cpp
    The extra newline throws an exception.

--- examples/json_lines.cpp
+++ examples/json_lines.cpp
@@ -11,8 +11,7 @@ int main()
     input << R"({"name": "Gilbert", "wins": [["straight", "7♣"], ["one pair", "10♥"]]}
 {"name": "Alexa", "wins": [["two pair", "4♠"], ["two pair", "9♠"]]}
 {"name": "May", "wins": []}
-{"name": "Deloise", "wins": [["three of a kind", "5♣"]]}
-)";
+{"name": "Deloise", "wins": [["three of a kind", "5♣"]]})";
 
     std::string line;
     while (std::getline(input, line))
