c15bc3caf8a605f4eb6e877cd8f2e29af6b91169
Niels Lohmann <mail@nlohmann.me>
2023-11-28 20:30:36 +0100

    :bookmark: set version to 3.11.3

--- tests/src/unit-no-mem-leak-on-adl-serialize.cpp
+++ tests/src/unit-no-mem-leak-on-adl-serialize.cpp
@@ -3,7 +3,6 @@
 // |  |  |__   |  |  | | | |  version 3.11.3
 // |_____|_____|_____|_|___|  https://github.com/nlohmann/json
 //
-// SPDX-FileCopyrightText: 2013-2022 Niels Lohmann <https://nlohmann.me>
 // SPDX-FileCopyrightText: 2013-2023 Niels Lohmann <https://nlohmann.me>
 // SPDX-License-Identifier: MIT
 
