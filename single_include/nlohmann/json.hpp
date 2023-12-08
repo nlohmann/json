1402ca486594e95ae152617ecfed4770ae90603f
Juan Carlos Arevalo Baeza (JCAB) <jcab@ntdev.microsoft.com>
2023-12-08 14:02:53 -0800

    Add the fix to the single_include
    I ran `make pretty` but that modified 20 files, performing a significant amount of indentation changes, none of them related to my change.
    I ran `make amalgamate`, but that did nothing. Apparently, the make rule won't run if the single_include files have already been updated by `make pretty`.
    I forced `make amalgamate` to do the work by touching the file with the fix.
    I then decided to keep just the minimal needed change: the addition of the fix to the single_include file.
    
    I just am not conversant enough in Linux to know whether I installed astyle correctly (had to clone the source from a beta branch and build, in order to get support for `--squeeze-lines`).

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -5697,7 +5697,14 @@ template<typename BasicJsonType, typename EnumType,
 inline void to_json(BasicJsonType& j, EnumType e) noexcept
 {
     using underlying_type = typename std::underlying_type<EnumType>::type;
-    external_constructor<value_t::number_integer>::construct(j, static_cast<underlying_type>(e));
+    if (std::is_unsigned<underlying_type>::value)
+    {
+        external_constructor<value_t::number_unsigned>::construct(j, static_cast<underlying_type>(e));
+    }
+    else
+    {
+        external_constructor<value_t::number_integer>::construct(j, static_cast<underlying_type>(e));
+    }
 }
 #endif  // JSON_DISABLE_ENUM_SERIALIZATION
 
