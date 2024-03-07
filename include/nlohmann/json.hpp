65b9b38ff33829b43d15488d84413233a34d63fb
Fredrik Sandhei <fredrik.sandhei@gmail.com>
2024-03-07 20:53:52 +0100

    Fix compile errors on other versions of clang

--- include/nlohmann/json.hpp
+++ include/nlohmann/json.hpp
@@ -3696,8 +3696,8 @@ class basic_json // NOLINT(cppcoreguidelines-special-member-functions,hicpp-spec
     //              std::is_scalar<ScalarType>::value, int>::type = 0>
     /// @brief comparison: equal
     /// @sa https://json.nlohmann.me/api/basic_json/operator_eq/
-    template <typename T,
-              std::enable_if<detail::json_compatible_type<T, basic_json_t>::value, int>::type = 0>
+    template <typename T>
+    requires detail::json_compatible_type<T, basic_json_t>::value
     bool operator==(T rhs) const noexcept
     {
         return *this == basic_json(rhs);
