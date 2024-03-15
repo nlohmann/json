91be9e017513b346af3d0a71615653f01ba1df4e
Niels Lohmann <mail@nlohmann.me>
2024-03-15 19:24:59 +0100

    :rotating_light: fix warning

--- include/nlohmann/json.hpp
+++ include/nlohmann/json.hpp
@@ -1214,11 +1214,11 @@ class basic_json // NOLINT(cppcoreguidelines-special-member-functions,hicpp-spec
     basic_json(basic_json&& other) noexcept
     // check that passed value is valid (has to be done before forwarding)
         : json_base_class_t((other.assert_invariant(false), std::forward<json_base_class_t>(other))),
-          m_data(std::move(other.m_data))
+          m_data(std::move(other.m_data))// NOLINT(bugprone-use-after-move,hicpp-invalid-access-moved)
     {
         // invalidate payload
-        other.m_data.m_type = value_t::null;
-        other.m_data.m_value = {};
+        other.m_data.m_type = value_t::null; // NOLINT(bugprone-use-after-move,hicpp-invalid-access-moved)
+        other.m_data.m_value = {};// NOLINT(bugprone-use-after-move,hicpp-invalid-access-moved)
 
         set_parents();
         assert_invariant();
