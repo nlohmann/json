6cd90434757c915f557ffb0f6802c683a14fe13f
Matthias Popp <mpopp@arri.at>
2025-03-17 16:22:19 +0100

    fix indentation, use the same indentation size at in class JsonValuePrinter
    the indentation size was a mix up, we should use always the same indentation size of 4
    
    Signed-off-by: Matthias Popp <mpopp@arri.at>

--- tools/gdb_pretty_printer/nlohmann-json.py
+++ tools/gdb_pretty_printer/nlohmann-json.py
@@ -15,18 +15,18 @@ class JsonValuePrinter:
 
 def json_lookup_function(val):
     if m := ns_pattern.fullmatch(str(val.type.strip_typedefs().name)):
-      name = m.group('name')
-      if name and name.startswith('basic_json<') and name.endswith('>'):
-          m = ns_pattern.fullmatch(str(val["m_data"]['m_type']))
-          t = m.group('name')
-          if t and t.startswith('detail::value_t::'):
-              try:
-                  union_val = val['m_data']['m_value'][t.replace('detail::value_t::', '', 1)]
-                  if union_val.type.code == gdb.TYPE_CODE_PTR:
-                      return gdb.default_visualizer(union_val.dereference())
-                  else:
-                      return JsonValuePrinter(union_val)
-              except Exception:
-                  return JsonValuePrinter(val["m_data"]['m_type'])
+        name = m.group('name')
+        if name and name.startswith('basic_json<') and name.endswith('>'):
+            m = ns_pattern.fullmatch(str(val["m_data"]['m_type']))
+            t = m.group('name')
+            if t and t.startswith('detail::value_t::'):
+                try:
+                    union_val = val['m_data']['m_value'][t.replace('detail::value_t::', '', 1)]
+                    if union_val.type.code == gdb.TYPE_CODE_PTR:
+                        return gdb.default_visualizer(union_val.dereference())
+                    else:
+                        return JsonValuePrinter(union_val)
+                except Exception:
+                    return JsonValuePrinter(val["m_data"]['m_type'])
 
 gdb.pretty_printers.append(json_lookup_function)
