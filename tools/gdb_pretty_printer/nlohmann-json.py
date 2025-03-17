2fe9f3d33139ca5e1b5c2f023e12ecd93f0ab854
Matthias Popp <mpopp@arri.at>
2025-03-17 16:24:56 +0100

    add variables for easier access
    Signed-off-by: Matthias Popp <mpopp@arri.at>

--- tools/gdb_pretty_printer/nlohmann-json.py
+++ tools/gdb_pretty_printer/nlohmann-json.py
@@ -17,17 +17,19 @@ def json_lookup_function(val):
     if m := ns_pattern.fullmatch(str(val.type.strip_typedefs().name)):
         name = m.group('name')
         if name and name.startswith('basic_json<') and name.endswith('>'):
-            m = ns_pattern.fullmatch(str(val["m_data"]['m_type']))
+            m_data = val['m_data']
+            m_type = m_data['m_type']
+            m = ns_pattern.fullmatch(str(m_type))
             t = m.group('name')
             prefix = 'detail::value_t::'
             if t and t.startswith(prefix):
                 try:
-                    union_val = val['m_data']['m_value'][t.replace(prefix, '', 1)]
+                    union_val = m_data['m_value'][t.replace(prefix, '', 1)]
                     if union_val.type.code == gdb.TYPE_CODE_PTR:
                         return gdb.default_visualizer(union_val.dereference())
                     else:
                         return JsonValuePrinter(union_val)
                 except Exception:
-                    return JsonValuePrinter(val["m_data"]['m_type'])
+                    return JsonValuePrinter(m_type)
 
 gdb.pretty_printers.append(json_lookup_function)
