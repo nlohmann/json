e17266a9be062412f1e97549873b86d203484bd3
Niels Lohmann <mail@nlohmann.me>
2025-04-14 16:09:02 +0200

    :alembic: add C++ standard library as matrix option
    Signed-off-by: Niels Lohmann <mail@nlohmann.me>

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -647,6 +647,31 @@ add_custom_target(ci_test_compiler_default
     COMMENT "Compile and test with default C++ compiler"
 )
 
+add_custom_target(ci_test_compiler_default_libcxx
+    COMMAND ${CMAKE_COMMAND}
+        -DCMAKE_BUILD_TYPE=Debug -GNinja
+        -DJSON_BuildTests=ON -DJSON_FastTests=ON
+        -S${PROJECT_SOURCE_DIR} -B${PROJECT_BINARY_DIR}/build_compiler_default
+        -DCMAKE_CXX_FLAGS="-stdlib=libc++"
+        -DCMAKE_EXE_LINKER_FLAGS="-lc++abi"
+        ${ADDITIONAL_FLAGS}
+    COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}/build_compiler_default --parallel ${N}
+    COMMAND cd ${PROJECT_BINARY_DIR}/build_compiler_default && ${CMAKE_CTEST_COMMAND} --parallel ${N} --exclude-regex "test-unicode" -LE git_required --output-on-failure
+    COMMENT "Compile and test with default C++ compiler (libc++)"
+)
+
+add_custom_target(ci_test_compiler_default_libstdcxx
+    COMMAND ${CMAKE_COMMAND}
+        -DCMAKE_BUILD_TYPE=Debug -GNinja
+        -DJSON_BuildTests=ON -DJSON_FastTests=ON
+        -S${PROJECT_SOURCE_DIR} -B${PROJECT_BINARY_DIR}/build_compiler_default
+        -DCMAKE_CXX_FLAGS="-stdlib=libstdc++"
+        ${ADDITIONAL_FLAGS}
+    COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}/build_compiler_default --parallel ${N}
+    COMMAND cd ${PROJECT_BINARY_DIR}/build_compiler_default && ${CMAKE_CTEST_COMMAND} --parallel ${N} --exclude-regex "test-unicode" -LE git_required --output-on-failure
+    COMMENT "Compile and test with default C++ compiler (libstdc++)"
+)
+
 ###############################################################################
 # CUDA example
 ###############################################################################
