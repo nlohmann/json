3b0f9e51b3e84fdace0750bcdd71d54fbaa317a8
Niels Lohmann <mail@nlohmann.me>
2025-05-30 08:12:10 +0200

    :rotating_light: suppress warnings
    Signed-off-by: Niels Lohmann <mail@nlohmann.me>

--- cmake/clang_flags.cmake
+++ cmake/clang_flags.cmake
@@ -3,6 +3,7 @@
 # -Wno-c++98-compat-pedantic      The library targets C++11.
 # -Wno-deprecated-declarations    The library contains annotations for deprecated functions.
 # -Wno-extra-semi-stmt            The library uses assert which triggers this warning.
+# -Wno-nrvo                       Doctest triggers this warning.
 # -Wno-padded                     We do not care about padding warnings.
 # -Wno-covered-switch-default     All switches list all cases and a default case.
 # -Wno-unsafe-buffer-usage        Otherwise Doctest would not compile.
@@ -14,6 +15,7 @@ set(CLANG_CXXFLAGS
     -Wno-c++98-compat-pedantic
     -Wno-deprecated-declarations
     -Wno-extra-semi-stmt
+    -Wno-nrvo
     -Wno-padded
     -Wno-covered-switch-default
     -Wno-unsafe-buffer-usage
