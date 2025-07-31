97c742a334a88cc9f92647b45686bc0b1dca6ff5
Hitgirl <hitgirl@Hitgirls-MacBook-Air.local>
2025-07-31 17:56:04 +0530

    refactor: move cross-compiling check before cached output detection
    Signed-off-by: Hitgirl <hitgirl@Hitgirls-MacBook-Air.local>

--- cmake/download_test_data.cmake
+++ cmake/download_test_data.cmake
@@ -68,10 +68,10 @@ string(REGEX REPLACE "[ ]*\n" "; " CXX_VERSION_RESULT "${CXX_VERSION_RESULT}")
 message(STATUS "Compiler: ${CXX_VERSION_RESULT}")
 
 # determine used C++ standard library (for debug and support purposes)
-if(NOT DEFINED LIBCPP_VERSION_OUTPUT_CACHED)
-    if(CMAKE_CROSSCOMPILING)
-        set(LIBCPP_VERSION_OUTPUT "could not be detected due to cross-compiling")
-    else()
+if(CMAKE_CROSSCOMPILING)
+    set(LIBCPP_VERSION_OUTPUT_CACHED "could not be detected due to cross-compiling" CACHE STRING "Detected C++ standard library version")
+else()
+    if(NOT DEFINED LIBCPP_VERSION_OUTPUT_CACHED)
         try_run(RUN_RESULT_VAR COMPILE_RESULT_VAR
             "${CMAKE_BINARY_DIR}" SOURCES "${CMAKE_SOURCE_DIR}/cmake/detect_libcpp_version.cpp"
             RUN_OUTPUT_VARIABLE LIBCPP_VERSION_OUTPUT 
@@ -81,8 +81,8 @@ if(NOT DEFINED LIBCPP_VERSION_OUTPUT_CACHED)
             set(LIBCPP_VERSION_OUTPUT "Unknown")
             message(AUTHOR_WARNING "Failed to compile cmake/detect_libcpp_version to detect the used C++ standard library. This does not affect the library or the test cases. Please still create an issue at https://github.com/nlohmann/json to investigate this.\n${LIBCPP_VERSION_COMPILE_OUTPUT}")
         endif()
+        set(LIBCPP_VERSION_OUTPUT_CACHED "${LIBCPP_VERSION_OUTPUT}" CACHE STRING "Detected C++ standard library version")
     endif()
-    set(LIBCPP_VERSION_OUTPUT_CACHED "${LIBCPP_VERSION_OUTPUT}" CACHE STRING "Detected C++ standard library version")
 endif()
      
 
