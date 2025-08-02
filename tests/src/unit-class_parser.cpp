b84be267844ce46a36ee50d47744ec0a81bdc10f
Nikhil <nikhilreddydev@gmail.com>
2025-08-02 14:11:39 +0530

    ignore undefined-behaviour-sanitizer
    Signed-off-by: Nikhil <nikhilreddydev@gmail.com>

--- tests/src/unit-class_parser.cpp
+++ tests/src/unit-class_parser.cpp
@@ -1648,10 +1648,12 @@ TEST_CASE("parser class")
             {
                 const std::string s = "some_string";
                 SaxCountdown* p = nullptr;
+# if !(defined(__has_feature) && __has_feature(undefined_behaviour_sanitizer))
                 CHECK_THROWS_WITH_AS(json::sax_parse(s, p), "[json.exception.other_error.502] SAX handler must not be null", json::other_error&); // NOLINT(clang-analyzer-core.NonNullParamChecker)
                 CHECK_THROWS_WITH_AS(json::sax_parse(s.begin(), s.end(), p), "[json.exception.other_error.502] SAX handler must not be null", json::other_error&); // NOLINT(clang-analyzer-core.NonNullParamChecker)
                 CHECK_THROWS_WITH_AS(json::sax_parse(nlohmann::detail::span_input_adapter(s.c_str(), s.size()), p), "[json.exception.other_error.502] SAX handler must not be null", json::other_error&); // NOLINT(clang-analyzer-core.NonNullParamChecker)
             }
+#endif
 
             SECTION("valid sax handler")
             {
