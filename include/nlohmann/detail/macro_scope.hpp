88e36cbf2a89d58943f3d20ea88fa6610636a76c
Ash-Jose <ashlinjose986@outlook.com>
2025-11-05 21:46:04 +0530

    fix: include exceptions.hpp for type_error in enum strict macro Signed-off-by: Ash-Jose <ashlinjose986@outlook.com>

--- include/nlohmann/detail/macro_scope.hpp
+++ include/nlohmann/detail/macro_scope.hpp
@@ -16,6 +16,7 @@
 // You MUST include macro_unscope.hpp at the end of json.hpp to undef all of them
 
 #include <nlohmann/detail/abi_macros.hpp>
+#include <nlohmann/detail/exceptions.hpp> // for type_error
 
 // exclude unsupported compilers
 #if !defined(JSON_SKIP_UNSUPPORTED_COMPILER_CHECK)
