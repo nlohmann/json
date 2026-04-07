ad9880ddf5e90724e444a3cf756e01c4e2e6e170
Samaresh Kumar Singh <ssam3003@gmail.com>
2026-04-07 18:16:35 -0500

    fix: remove compiler-dependent assertions from #5074 regression test
    Signed-off-by: Samaresh Kumar Singh <ssam3003@gmail.com>

--- tests/src/unit-regression2.cpp
+++ tests/src/unit-regression2.cpp
@@ -1188,25 +1188,14 @@ TEST_CASE_TEMPLATE("issue #4798 - nlohmann::json::to_msgpack() encode float NaN 
     CHECK(json::from_cbor(cbor_z_3).get<T>() == -std::numeric_limits<T>::infinity());
 }
 
-TEST_CASE("regression test #5074 - single-element brace init default behavior")
+TEST_CASE("regression test #5074 - portable workaround for single-element brace init")
 {
     json const j_obj = {{"key", "value"}};
-    json const j_arr = {1, 2, 3};
 
-    json const j1{j_obj};
-    CHECK(j1.is_array());
-    CHECK(j1.size() == 1);
-    CHECK(j1[0] == j_obj);
-
-    json const j2{j_arr};
-    CHECK(j2.is_array());
-    CHECK(j2.size() == 1);
-    CHECK(j2[0] == j_arr);
-
-    json const j3 = json::array({j_obj});
-    CHECK(j3.is_array());
-    CHECK(j3.size() == 1);
-    CHECK(j3[0] == j_obj);
+    json const j = json::array({j_obj});
+    CHECK(j.is_array());
+    CHECK(j.size() == 1);
+    CHECK(j[0] == j_obj);
 }
 
 #if JSON_BRACE_INIT_COPY_SEMANTICS
