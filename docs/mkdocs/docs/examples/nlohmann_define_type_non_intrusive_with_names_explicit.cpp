9ed35d1d22fe6fc9ac133d5551ffcc3652917a5a
George Sedov <radist.morse@gmail.com>
2026-05-11 15:46:17 +0200

    astyle changes

--- docs/mkdocs/docs/examples/nlohmann_define_type_non_intrusive_with_names_explicit.cpp
+++ docs/mkdocs/docs/examples/nlohmann_define_type_non_intrusive_with_names_explicit.cpp
@@ -14,17 +14,19 @@ struct person
 };
 
 template <typename BasicJsonType, nlohmann::detail::enable_if_t<nlohmann::detail::is_basic_json<BasicJsonType>::value, int> = 0>
-void to_json(BasicJsonType& nlohmann_json_j, const person& nlohmann_json_t) {
-  nlohmann_json_j["json_name"] = nlohmann_json_t.name;
-  nlohmann_json_j["json_address"] = nlohmann_json_t.address;
-  nlohmann_json_j["json_age"] = nlohmann_json_t.age;
+void to_json(BasicJsonType& nlohmann_json_j, const person& nlohmann_json_t)
+{
+    nlohmann_json_j["json_name"] = nlohmann_json_t.name;
+    nlohmann_json_j["json_address"] = nlohmann_json_t.address;
+    nlohmann_json_j["json_age"] = nlohmann_json_t.age;
 }
 
 template <typename BasicJsonType, nlohmann::detail::enable_if_t<nlohmann::detail::is_basic_json<BasicJsonType>::value, int> = 0>
-void from_json(const BasicJsonType& nlohmann_json_j, person& nlohmann_json_t) {
-  nlohmann_json_j.at("json_name").get_to(nlohmann_json_t.name);
-  nlohmann_json_j.at("json_address").get_to(nlohmann_json_t.address);
-  nlohmann_json_j.at("json_age").get_to(nlohmann_json_t.age);
+void from_json(const BasicJsonType& nlohmann_json_j, person& nlohmann_json_t)
+{
+    nlohmann_json_j.at("json_name").get_to(nlohmann_json_t.name);
+    nlohmann_json_j.at("json_address").get_to(nlohmann_json_t.address);
+    nlohmann_json_j.at("json_age").get_to(nlohmann_json_t.age);
 }
 } // namespace ns
 
