6d9e0a8a169552f8ff8523019bd5ccb0ebe645a2
Caillin Nugent <caillinn@student.unimelb.edu.au>
2026-05-13 13:59:05 +1000

    refactor(macros): make JSON_SERIALIZE_ENUM_STRICT use JSON_THROW - added templated wrapper function to fix scope error in calling JSON_THROW
    Signed-off-by: Caillin Nugent <caillinn@student.unimelb.edu.au>

--- include/nlohmann/detail/macro_scope.hpp
+++ include/nlohmann/detail/macro_scope.hpp
@@ -253,6 +253,19 @@
         e = ((it != std::end(m)) ? it : std::begin(m))->first;                                  \
     }
 
+
+
+/*!
+@brief function to wrap JSON_THROW_MACRO - NLOHMANN_SERIALIZE_ENUM_STRICT has a
+	   compilation warning about there being no arguments to JSON_THROW that depend on
+	   template arguments otherwise
+*/
+template<typename ExceptionType>
+void templated_json_throw(ExceptionType exception)
+{
+	JSON_THROW(exception);
+}
+
 /*!
 @brief macro to briefly define a mapping between an enum and JSON with exception
        on invalid input
@@ -273,7 +286,7 @@
             return ej_pair.first == e;                                                          \
         });                                                                                     \
         if (it != std::end(m)) j = it->second;                                                  \
-        else throw nlohmann::detail::out_of_range::create(403, "enum value out of range", nullptr);\
+		else templated_json_throw<nlohmann::detail::out_of_range>(nlohmann::detail::out_of_range::create(403,"enum value out of range",nullptr)); \
     }                                                                                           \
     template<typename BasicJsonType>                                                            \
     inline void from_json(const BasicJsonType& j, ENUM_TYPE& e)                                 \
@@ -288,7 +301,7 @@
             return ej_pair.second == j;                                                         \
         });                                                                                     \
         if (it != std::end(m)) e = it->first;                                                   \
-        else throw nlohmann::detail::out_of_range::create(403, "enum value out of range", nullptr);\
+		else templated_json_throw<nlohmann::detail::out_of_range>(nlohmann::detail::out_of_range::create(403,"enum value out of range",nullptr)); \
     }
 
 
