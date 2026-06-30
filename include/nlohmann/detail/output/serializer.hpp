526c8c50795aa75991997c054ea2ffb2a86cbfb7
Niels Lohmann <mail@nlohmann.me>
2026-06-30 19:03:23 +0200

    :recycle: check for truncation
    Signed-off-by: Niels Lohmann <mail@nlohmann.me>

--- include/nlohmann/detail/output/serializer.hpp
+++ include/nlohmann/detail/output/serializer.hpp
@@ -125,8 +125,7 @@ class serializer
                     o->write_characters("{\n", 2);
 
                     // variable to hold indentation for recursive calls
-                    const auto new_indent = current_indent + indent_step;
-                    reserve_indent(new_indent);
+                    const auto new_indent = reserve_indent(current_indent, indent_step);
 
                     // first n-1 elements
                     auto i = val.m_data.m_value.object->cbegin();
@@ -195,8 +194,7 @@ class serializer
                     o->write_characters("[\n", 2);
 
                     // variable to hold indentation for recursive calls
-                    const auto new_indent = current_indent + indent_step;
-                    reserve_indent(new_indent);
+                    const auto new_indent = reserve_indent(current_indent, indent_step);
 
                     // first n-1 elements
                     for (auto i = val.m_data.m_value.array->cbegin();
@@ -253,8 +251,7 @@ class serializer
                     o->write_characters("{\n", 2);
 
                     // variable to hold indentation for recursive calls
-                    const auto new_indent = current_indent + indent_step;
-                    reserve_indent(new_indent);
+                    const auto new_indent = reserve_indent(current_indent, indent_step);
 
                     o->write_characters(indent_string.c_str(), new_indent);
 
@@ -365,22 +362,30 @@ class serializer
     }
 
     /*!
-    @brief grow the indentation string so it can hold at least @a new_indent characters
+    @brief compute the next indentation level and grow the indentation string
 
-    The indentation string is grown by doubling its size, but at least to
-    @a new_indent characters, so that it can be used as a source for writing
-    @a new_indent indentation characters. The newly added characters use the
-    configured @ref indent_char.
+    Computes the new indentation level @a current_indent + @a indent_step and
+    grows the indentation string (by doubling its size, but at least to the new
+    level) so that it can be used as a source for writing that many indentation
+    characters. The newly added characters use the configured @ref indent_char.
 
-    @param[in] new_indent  the required number of indentation characters
+    @param[in] current_indent  the current indentation level
+    @param[in] indent_step     the number of characters to indent per level
+
+    @return the new indentation level @a current_indent + @a indent_step
     */
-    void reserve_indent(const std::size_t new_indent)
+    unsigned int reserve_indent(const unsigned int current_indent, const unsigned int indent_step)
     {
+        const unsigned int new_indent = current_indent + indent_step;
+        // a very large indent_step can wrap the unsigned accumulation on deep
+        // nesting, which would silently truncate the indentation
+        JSON_ASSERT(new_indent >= current_indent);
         if (JSON_HEDLEY_UNLIKELY(indent_string.size() < new_indent))
         {
-            indent_string.resize((std::max)(indent_string.size() * 2, new_indent), indent_char);
+            indent_string.resize((std::max)(indent_string.size() * 2, static_cast<std::size_t>(new_indent)), indent_char);
         }
         JSON_ASSERT(indent_string.size() >= new_indent);
+        return new_indent;
     }
 
   JSON_PRIVATE_UNLESS_TESTED:
