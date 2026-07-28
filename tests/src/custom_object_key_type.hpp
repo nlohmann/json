61e2dcb98935bac45cda1605835f79436b4f4469
alexprabhat99 <alexpbara@gmail.com>
2026-07-28 17:16:25 +0530

    avoid allocating temporary basic_json for cbor and msgpack object keys
    Signed-off-by: alexprabhat99 <alexpbara@gmail.com>

---
+++ tests/src/custom_object_key_type.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include <nlohmann/json.hpp>
+
+namespace custom_object_key_test
+{
+class key
+{
+  public:
+    key() = default;
+
+    key(const char* value)
+        : m_value(value)
+    {}
+
+    key(std::string value)
+        : m_value(std::move(value))
+    {}
+
+    operator std::string() const
+    {
+        return m_value;
+    }
+
+    friend bool operator<(const key& lhs, const key& rhs)
+    {
+        return lhs.m_value < rhs.m_value;
+    }
+
+  private:
+    std::string m_value;
+};
+
+template<typename Key, typename Value, typename Compare, typename Allocator>
+class object
+    : public std::map<
+          key,
+          Value,
+          std::less<key>,
+          typename std::allocator_traits<Allocator>::template rebind_alloc<
+              std::pair<const key, Value>>>
+{
+  private:
+    using allocator_type =
+        typename std::allocator_traits<Allocator>::template rebind_alloc<
+            std::pair<const key, Value>>;
+
+    using base_type =
+        std::map<key, Value, std::less<key>, allocator_type>;
+
+  public:
+    using base_type::base_type;
+};
+
+using json = nlohmann::basic_json<object>;
+} // namespace custom_object_key_test
