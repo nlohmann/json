3678634bf2371720dbbed2ebe5ed3e70ddcea364
Niels Lohmann <mail@nlohmann.me>
2026-09-05 16:34:21 +0200

    Fix swap(array_t&)/swap(object_t&) to update parent pointers under JSON_DIAGNOSTICS
    Both overloads swapped the underlying container storage but never called
    set_parents(), leaving elements moved into *this with stale m_parent
    pointers (typically nullptr from the free-standing array_t/object_t).
    This produced wrong JSON Pointer paths in diagnostic messages and could
    trip assert_invariant() on subsequent copies. Mirrors the fix already
    applied in swap(reference other).
    
    Signed-off-by: Niels Lohmann <mail@nlohmann.me>

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -24992,6 +24992,7 @@ class basic_json // NOLINT(cppcoreguidelines-special-member-functions,hicpp-spec
         {
             using std::swap;
             swap(*(m_data.m_value.array), other);
+            set_parents();
         }
         else
         {
@@ -25008,6 +25009,7 @@ class basic_json // NOLINT(cppcoreguidelines-special-member-functions,hicpp-spec
         {
             using std::swap;
             swap(*(m_data.m_value.object), other);
+            set_parents();
         }
         else
         {
