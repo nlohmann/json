c655be6668a354d1d05f933ca5cd6dd7f9cfec50
dajiaohuang <mikewushuwen@outlook.com>
2026-09-09 04:57:07 +0800

    Remove unused iomanip include
    Signed-off-by: dajiaohuang <mikewushuwen@outlook.com>

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -18970,7 +18970,6 @@ NLOHMANN_JSON_NAMESPACE_END
 #include <cstdio> // snprintf
 #include <limits> // numeric_limits
 #include <string> // string, char_traits
-#include <iomanip> // setfill, setw
 #include <type_traits> // is_same
 #include <utility> // move
 
