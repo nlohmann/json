c409e16734dc5253c6d30e79c27d7335234f3373
dsp0redy <saipraneethreddy.dadireddy@gmail.com>
2026-09-09 20:43:48 +0530

    addressed review comments
    Signed-off-by: dsp0redy <saipraneethreddy.dadireddy@gmail.com>

--- single_include/nlohmann/json.hpp
+++ single_include/nlohmann/json.hpp
@@ -17586,6 +17586,11 @@ class binary_writer
                     oa->write_character(to_char_type(0xDB));
                     write_number(static_cast<std::uint32_t>(N));
                 }
+                else
+                {
+                    JSON_THROW(out_of_range::create(412, concat("MessagePack size ", std::to_string(N), " exceeds maximum of ",
+                                                    std::to_string((std::numeric_limits<std::uint32_t>::max)())), &j));
+                }
 
                 // step 2: write the string
                 oa->write_characters(
@@ -17614,6 +17619,11 @@ class binary_writer
                     // array 32
                     oa->write_character(to_char_type(0xDD));
                     write_number(static_cast<std::uint32_t>(N));
+                }
+                else
+                {
+                    JSON_THROW(out_of_range::create(412, concat("MessagePack size ", std::to_string(N), " exceeds maximum of ",
+                                                    std::to_string((std::numeric_limits<std::uint32_t>::max)())), &j));
                 }
 
                 // step 2: write each element
@@ -17692,6 +17702,11 @@ class binary_writer
                     oa->write_character(to_char_type(output_type));
                     write_number(static_cast<std::uint32_t>(N));
                 }
+                else
+                {
+                    JSON_THROW(out_of_range::create(412, concat("MessagePack size ", std::to_string(N), " exceeds maximum of ",
+                                                    std::to_string((std::numeric_limits<std::uint32_t>::max)())), &j));
+                }
 
                 // step 1.5: if this is an ext type, write the subtype
                 if (use_ext)
@@ -17727,6 +17742,11 @@ class binary_writer
                     // map 32
                     oa->write_character(to_char_type(0xDF));
                     write_number(static_cast<std::uint32_t>(N));
+                }
+                else
+                {
+                    JSON_THROW(out_of_range::create(412, concat("MessagePack size ", std::to_string(N), " exceeds maximum of ",
+                                                    std::to_string((std::numeric_limits<std::uint32_t>::max)())), &j));
                 }
 
                 // step 2: write each element
@@ -18946,7 +18966,6 @@ class binary_writer
 
 }  // namespace detail
 NLOHMANN_JSON_NAMESPACE_END
-
 // #include <nlohmann/detail/output/output_adapters.hpp>
 
 // #include <nlohmann/detail/output/serializer.hpp>
